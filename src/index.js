0f0e82b40f5607b6bfce8983161e26018b0b5c2a
BennyLouie <bennylouie1412@gmail.com>
2020-02-12 16:42:43 -0500

    created store

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import reducer from './reducers/reducer'
+
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-    <Provider>
+    <Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
