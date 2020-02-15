8098636647057e3a598d228e1b2199421a80474c
BennyLouie <bennylouie1412@gmail.com>
2020-02-15 17:57:38 -0500

    color changing

--- src/components/MyStock.js
+++ src/components/MyStock.js
@@ -1,10 +1,23 @@
 import React from 'react'
 
+//Function for Color Changing:
+function colorChange(currentPrice, openingPrice) {
+    if (currentPrice > openingPrice) {
+        return 'green'
+    }
+    else if (currentPrice < openingPrice) {
+        return 'red'
+    }
+    else {
+        return 'grey'
+    }
+}
+
 export default function MyStock(props) {
     console.log(props)
     return (
         <div className='stock ui fluid card'>
-            <h4>{props.name} - {props.shares} Shares ${Number(Math.round((props.current_price * props.shares) +'e2') +'e-2')}</h4>
+            <h4><span data-status={colorChange(props.current_price, props.opening_price)}>{props.name}</span> - {props.shares} Shares <span data-status={colorChange(props.current_price, props.opening_price)}>${Number(Math.round((props.current_price * props.shares) +'e2') +'e-2')}</span></h4>
         </div>
     )
 }
