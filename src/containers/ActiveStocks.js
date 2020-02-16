991191e056eb6a3c6c71ba952cbeb7591d464aad
BennyLouie <bennylouie1412@gmail.com>
2020-02-16 15:42:47 -0500

    display 'owned' if stock in market is owned

--- src/containers/ActiveStocks.js
+++ src/containers/ActiveStocks.js
@@ -3,11 +3,15 @@ import Stock from '../components/Stock'
 
 export default function ActiveStocks(props) {
     // console.log(props)
+    let stockNames = props.stocks.map( s => s.name)
+    // console.log(stockNames)
+    let marketNames = props.market.map(m => m.symbol)
+    // console.log(marketNames)
     return (
         <div className='stocksdisplay'>
             <h1 className='title'>Most Active Stocks</h1>
             <div className='stockscontainer below-title ui cards'>
-                {props.market.map( (stock, idx) => <Stock key={idx} {...stock} />)}
+                {props.market.map( (stock, idx) => <Stock key={idx} {...stock} stockNames={stockNames}/>)}
             </div>
         </div>
     )
