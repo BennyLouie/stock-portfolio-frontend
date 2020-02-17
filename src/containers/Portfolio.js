5f7950e91606d47f36667d8d1d729a4da7d04d64
BennyLouie <bennylouie1412@gmail.com>
2020-02-17 14:16:12 -0500

    added portfolio total

--- src/containers/Portfolio.js
+++ src/containers/Portfolio.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import MyStock from '../components/MyStock'
 
+function totalPrice(stocks) {
+    let prices = stocks.map(s => s.current_price * s.shares)
+    let total = Math.round((prices.reduce((t, p) => t + p, 0) * 100) / 100).toFixed(2)
+    return total
+}
+
 export default function Portfolio(props) {
+    console.log(props.stocks)
+    console.log(totalPrice(props.stocks))
     return (
         <div className='stocksdisplay'>
-            <h1 className='title'>Portfolio</h1>
+            <h1 className='title'>Portfolio (${totalPrice(props.stocks)})</h1>
             <div className='stockscontainer below-title ui cards'>
                 { props.stocks.map((stock, idx) => <MyStock key={idx} {...stock}  />)}
             </div>
